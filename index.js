7f892e2166a470c4f263d7da4af5dabc9d52ea66
Light Yagami <sagnikghosh@zohomail.in>
2023-03-30 01:54:51 +0530

    updated index.js

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const config = require('./utils/config')
 
 app.listen(config.PORT, () => {
     console.log(`Listening on ${config.PORT}`)
+    console.log(`Visit http://localhost:${config.PORT}/\n`)
 })
